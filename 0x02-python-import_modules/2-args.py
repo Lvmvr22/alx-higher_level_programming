77a6774f719b3b4ba0bb383367580d187f534761
Lvmvr22 <lamarnyairo@gmail.com>
2021-04-29 22:29:22 +0300

    init commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
-    count = len(sys.argv) -1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    if len(argv) == 1:
+        print('{:d}'.format(len(argv)-1))
+
+    elif len(argv) > 1:
+        sum = 0
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print('{:d}'.format(sum))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
+    from sys import argv
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    if len(argv) == 1:
+        print('{:d}'.format(len(argv)-1))
+
+    elif len(argv) > 1:
+        sum = 0
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print('{:d}'.format(sum))
