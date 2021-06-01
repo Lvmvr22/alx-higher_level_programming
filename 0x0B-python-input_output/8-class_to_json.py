6ef17240756aa6886fc7da02dd256b100715812b
Lvmvr22 <lamarnyairo@gmail.com>
2021-06-02 00:52:51 +0300

    fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,11 @@
-#!/usr/bin/pythhon3
-""" returns a json representation of an object
+#!/usr/bin/python3
+"""
+    8-class_to_json: class_to_json()
 """
 
 
 def class_to_json(obj):
-    """returns a json representation of
-    the obj object"""
+    """
+        returns the dictionary description with simple data structure.
+    """
     return obj.__dict__
